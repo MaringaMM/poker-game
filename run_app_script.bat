2f3c91c58fa033c9e3edca3476a045bcb4d1de5e
Musa Maringa (ZA) <Musa.Maringa@absa.africa>
2022-07-01 12:51:56 +0200

    add run script

---
+++ run_app_script.bat
@@ -0,0 +1,6 @@
+#compile, test, install, package and run 
+
+cd C:\Users\%username%\Desktop\fivecard-drawapp
+#**************** Compile, Test, Install, package and run app **************"
+cmd /k mvn install exec:java -Dexec.mainClass=com.itq.PokerPlayer
+
